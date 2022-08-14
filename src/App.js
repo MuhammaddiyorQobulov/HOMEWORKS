6d2c53ea00a2ddf6de922b17a1bd3fd8b79bfe2c
MuhammaddiyorQobulov <muhammaddiyor20032808@gmail.com>
2022-08-14 19:37:17 +0500

    deault codes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React, { Component } from "react";
 import "./App.scss";
-import Slider from "./components/slider";
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Slider />
       </div>
     );
   }
