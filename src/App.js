1bd66c3edbd87a0a556a21909ca39b4f64ef0e0d
MuhammaddiyorQobulov <muhammaddiyor20032808@gmail.com>
2022-08-14 23:19:05 +0500

    ff

--- src/App.js
+++ src/App.js
@@ -2,10 +2,7 @@ import React, { Component } from "react";
 import "./App.scss";
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-      </div>
-    );
+    return <div className="App"></div>;
   }
 }
 
