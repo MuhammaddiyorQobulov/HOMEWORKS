ae820896cae9f47bea90f20aea5fc501e232abdc
MuhammaddiyorQobulov <muhammaddiyor20032808@gmail.com>
2022-08-14 19:36:27 +0500

    last codes

--- src/components/slider.jsx
+++ src/components/slider.jsx
@@ -1,11 +1,56 @@
 import React, { Component } from "react";
+import { ceil } from "lodash";
+import "./slider.scss";
 
 class Slider extends Component {
+  state = {
+    dot1: 20,
+    dot2: 55,
+  };
+  handleClick = (e) => {
+    const { dot1, dot2 } = this.state;
+    const width = e.pageX - 50;
+    const parcent = (width * 100) / e.target.clientWidth;
+    this.setState({
+      dot1:
+        Math.abs(parcent - dot1) < Math.abs(parcent - dot2) ? parcent : dot1,
+      dot2:
+        Math.abs(parcent - dot1) > Math.abs(parcent - dot2) ? parcent : dot2,
+    });
+    console.log(parcent);
+  };
+
   render() {
+    const { handleClick } = this;
+    const { dot1, dot2 } = this.state;
+
     return (
-      <div className="slider">
-        <h1>Slider</h1>
-      </div>
+      <>
+        <div
+          onClick={handleClick}
+          style={{
+            "--dot1": `${dot1}%`,
+            "--dot2": `${dot2}%`,
+          }}
+          className="slider"
+        >
+          <span className="dot1">
+            <nav className="dot1-value">{ceil(dot2, 1)}%</nav>
+          </span>
+          <span className="dot2">
+            <nav className="dot2-value">{ceil(dot1, 1)}%</nav>
+          </span>
+        </div>
+        <button
+          className="showDistance"
+          onClick={() => {
+            const distance = ceil(Math.abs(dot1 - dot2), 1);
+            alert(distance);
+          }}
+        >
+          Distance
+        </button>
+      </>
     );
   }
 }
