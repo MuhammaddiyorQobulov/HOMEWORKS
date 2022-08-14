f5902b09030fd680cca7bc3ebe8d36555a1cf531
MuhammaddiyorQobulov <muhammaddiyor20032808@gmail.com>
2022-08-14 16:54:13 +0500

    start codes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
+import React, { Component } from "react";
+import "./App.scss";
+import Slider from "./components/slider";
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Slider />
       </div>
-  );
+    );
+  }
 }
 
 export default App;
---
+++ src/components/slider.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Slider extends Component {
+  render() {
+    return (
+      <div className="slider">
+        <h1>Slider</h1>
+      </div>
+    );
+  }
+}
+
+export default Slider;
